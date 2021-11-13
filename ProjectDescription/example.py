8f335d40483dab26bc1e4758243fd0c7b9d381bb
unknown <31831508+0x0000000000000000000@users.noreply.github.com>
2021-11-13 23:05:33 +0200

    Update example.py

--- ProjectDescription/example.py
+++ ProjectDescription/example.py
@@ -8,3 +8,60 @@ def fib(n):    # write Fibonacci series up to n
                       
 def funcwithdef(name,university="aueb"):
        print name, " studies in ", university
+
+    
+import package
+import package from namespace as package_renamed
+
+#function call
+class C:
+    class D:
+        def meth(self):
+            pass
+Class = C
+
+Class.D.meth
+
+#parameter
+def func(foo,bar=None):
+    print(a+b)
+
+#Pythonic
+for piece in food:
+    print(piece)
+
+#Strings
+
+string1 = "String1"
+string2 = 'String2'
+
+#Arithmetic , Logical , Comparison Operators
+
+Num1 = 10
+Num2 = 20
+
+#comp_eqs
+Num1 = Num1
+Num1 -= Num2
+Num1 /= Num2
+#comp_types
+Num1 < Num2
+Num1 > Num2
+Num1 >= Num2
+Num1 <= Num2
+Num1 != Num2
+Num1 == Num2
+#and_or
+Num1 and Num2
+Num1 or Num2
+#max_min
+max(Num1,Num2)
+min(Num1,Num2)
+#math_types
+Num1 + Num2
+Num1 - Num2
+Num1 * Num2
+Num1 ** Num2
+Num1 / Num2
+Num1 % Num2
+
